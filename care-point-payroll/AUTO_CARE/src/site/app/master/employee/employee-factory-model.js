f0be436bbbf5a5a9f19511b56a48edc3883c0b96
Nidura Prageeth <niduraprageeth@gmail.com>
2017-12-01 18:10:46 +0530

    employee master file

--- care-point-payroll/AUTO_CARE/src/site/app/master/employee/employee-factory-model.js
+++ care-point-payroll/AUTO_CARE/src/site/app/master/employee/employee-factory-model.js
@@ -10,11 +10,30 @@
                         "addressLine2": null,
                         "addressLine3": null,
                         "mobile": null,
+                        "telNo": null,
+                        "email": null,
+                        "nic": null,
                         "branch": null,
+                        "civilStatus": null,
                         "type": null,
-                        "rol": null,
+                        "department": null,
+                        "joinedDate": null,
+                        "active": null,
+                        "confirm": null,
+                        "resigned": null,
                         "image": null,
-                        "bay": null
+                        "epfNo": null,
+                        "fingerPrintNo": null,
+                        "basicSalary": null,
+                        "basicForEpf": null,
+                        "payrollType": null,
+                        "epfStatus": null,
+                        "epfGrade": null,
+                        "epfApplicable": null,
+                        "otApplicable": null,
+                        "morningOt": null,
+                        "attendanceAllowance": null,
+                        "taxApplicable": null
                     };
                     return data;
                 };
